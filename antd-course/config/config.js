6937379e0d20aa1dcb0a7548e471b76d12b156ff
e <13696426353@163.com>
2021-03-10 09:02:11 +0800

    antd-course 1

---
+++ antd-course/config/config.js
@@ -0,0 +1,16 @@
+export default{
+    singular: true,
+    // plugins: [
+    //     ['umi-plugin-react', {
+    //         //添加配置
+    //         antd: true
+    //     }],
+    // ],
+    antd: {},
+    routes: [
+        {
+            path: '/',
+            component: './HelloWorld',
+        }
+    ],
+}
